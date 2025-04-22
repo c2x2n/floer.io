da1c73c96ecb39822ac42bd321bf4dc965a31e09
Camvanaa <3499880997@qq.com>
2025-04-23 00:23:28 +0800

    balance

--- common/src/constants.ts
+++ common/src/constants.ts
@@ -27,7 +27,7 @@ export const GameConstants = {
         spawnMaxY: 50,
         defaultSlot: 10,
         defaultPrepareSlot: 10,
-        defaultEquippedPetals: ["penta", "penta", "penta", "penta", "powder","penta", "penta", "penta", "penta","penta"],
+        defaultEquippedPetals: ["basic", "basic", "basic", "basic", "basic"],
         defaultPreparationPetals: [],
         defaultPetalDistance: 3.5,
         defaultPetalAttackingDistance: 6.5,
