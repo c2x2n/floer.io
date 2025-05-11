af280fe21836bc27ea685262f9373d520282c256
XNOR <162976879+XNORIGC@users.noreply.github.com>
2025-05-11 23:34:44 +0800

    Add comment

--- common/src/utils/misc.ts
+++ common/src/utils/misc.ts
@@ -1,3 +1,4 @@
+// https://discord.com/channels/668937237010055189/668939882416308274/1333528343601217567
 export function strokeColor(color: string): string {
   const s =
       (c:number) => Math.round(parseInt(color.slice(c * 2 + 1, c * 2 + 3), 16) * 0.81).toString(16).padStart(2, "0");
