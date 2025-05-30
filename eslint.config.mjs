eb8420b8350d3d0343beca1952e78989256a2512
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-05-30 21:31:35 +0800

    ci: removed tsconfigRootDir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,6 @@ export default tseslint.config(
         languageOptions: {
             parserOptions: {
                 project: "tsconfig.eslint.json",
-                tsconfigRootDir: import.meta.dirname,
                 warnOnUnsupportedTypeScriptVersion: false
             }
         },
